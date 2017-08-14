a6d0a13d12a6c894990433e47699262569a1bd8f
ScriptTiger <thescripttiger@gmail.com>
2017-08-14 20:29:57 +0800

    Repo initialized

---
+++ README.md
@@ -0,0 +1,5 @@
+# Repo Search
+Tool to instantly search all or specific files in any repo for keywords or phrases natively on windows extremely easily, with an option to output results to a file.
+
+You can easily make a shortcut to search a specific repo with one click by sending the root and files to search as parameters in a short one-line script:  
+@search.cmd "c:\blah\blah\myrepo" "*.txt *.c *.h"
---
+++ search.cmd
@@ -0,0 +1,60 @@
+@echo off
+
+rem =====
+rem Remember current directory for file output
+rem =====
+
+set DIR=%~dp0
+
+rem =====
+rem Change the current directory to the source directory to search
+rem =====
+
+if "%~1"=="" (
+set /p INPUT=Root directory to search? 
+) else (
+set INPUT=%~1)
+cd "%INPUT%"
+
+rem =====
+rem File types to search for
+rem =====
+
+if "%~2"=="" (
+set /p TYPES=Search which source files ^(i.e. "*.txt *.md" to search all txt and md files^)? 
+) else (
+set TYPES=%~2)
+
+rem =====
+rem Begin main script
+rem =====
+
+:0
+
+rem =====
+rem Set options for search and execute
+rem =====
+
+choice /m "Use regular expressions?"
+if %ERRORLEVEL%==1 (set FIND=findstr /r /s /i ) else (set FIND=findstr /s /i /c:)
+set /p INPUT=Search for: || exit /b
+set FIND=%FIND%"%INPUT%" %TYPES%^|more
+%FIND%
+
+rem =====
+rem Give option to output search results to a file in the current directory
+rem =====
+
+choice /m "Output results to file?"
+if %ERRORLEVEL%==1 (
+set /p INPUT=Name of file to export results to? && %FIND%>"%DIR%%INPUT%.txt"
+)
+
+rem =====
+rem Give option to open any source files of interest
+rem =====
+
+:1
+set /P INPUT=Open file: || goto 0
+write "%INPUT%"
+goto 1
